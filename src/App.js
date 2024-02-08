43abf8f8bd59c4e447131e9a79a5cad9637cb755
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2024-02-09 02:16:01 +0530

    04ImageSlider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Accordian from './components/accordian';
+import ImageSlider from './components/imageSlider';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 
@@ -14,7 +15,17 @@ function App() {
       {/* <RandomColor /> */}
 
       {/* Star Rating */}
-      <StarRating noOfStar={10}/>
+      {/* <StarRating noOfStar={10}/> */}
+
+      {/* Image Slider */}
+      {/* <ImageSlider /> */}
+
+      {/* Image slider component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }

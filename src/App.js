54c9d68ba2b0aff029ddeb1890843bb282abb7ad
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2024-02-08 05:57:36 +0530

    02randomColor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
 import './App.css';
 import Accordian from './components/accordian';
+import RandomColor from './components/random-color';
 
 function App() {
   return (
     <div className="App">
-      <Accordian />
+      {/* Accordian component */}
+      {/* <Accordian /> */}
+
+      {/* Random Color */}
+      <RandomColor />
     </div>
   );
 }
